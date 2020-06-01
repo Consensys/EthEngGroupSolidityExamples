f5db88d4ebe4fc91440d529af48d319e3f5573c6
Peter  Robinson <drinkcoffee@eml.cc>
2020-06-01 14:30:22 +1000

    Initial version
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

---
+++ evm/gen.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+rm -rf build
+
+BUILDDIR=./build
+BUILDDIRO=./build/optimize
+BUILDDIRN=./build/normal
+CONTRACTSDIR=contracts
+
+
+# compiling one file also compiles its dependendencies. We use overwrite to avoid the related warnings.
+solc $CONTRACTSDIR/Simple.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
+solc $CONTRACTSDIR/Memory.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
+solc $CONTRACTSDIR/Modifier.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
+solc $CONTRACTSDIR/ERC20.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
+solc $CONTRACTSDIR/Simple.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi -o $BUILDDIRN --overwrite
+# solc $CONTRACTSDIR/ERC20.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi -o $BUILDDIRN --overwrite
+ls $BUILDDIRO
+
