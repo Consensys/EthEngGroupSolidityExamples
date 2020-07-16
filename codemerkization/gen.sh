d1a8749d03af9946f277424cdd042e7ce3e3cf4e
Peter  Robinson <drinkcoffee@eml.cc>
2020-07-16 17:06:16 +1000

    Add code merklization
    Signed-off-by: Peter  Robinson <drinkcoffee@eml.cc>

--- codemerkization/gen.sh
+++ codemerkization/gen.sh
@@ -9,6 +9,7 @@ CONTRACTSDIR=contracts
 
 # compiling one file also compiles its dependendencies. We use overwrite to avoid the related warnings.
 solc $CONTRACTSDIR/ERC20.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
+solc $CONTRACTSDIR/Conditional.sol --allow-paths . --asm --asm-json --hashes --userdoc  --devdoc  --metadata --opcodes --bin --abi --optimize -o $BUILDDIRO --overwrite
 
 ls $BUILDDIRO
 
